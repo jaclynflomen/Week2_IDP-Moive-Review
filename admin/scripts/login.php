ad1e161d42ddc75c793754c3217207393ce9fee1
jaclynflomen <j_floman@fanshaweonline.ca>
2019-01-22 17:07:59 -0500

    prepare

--- admin/scripts/login.php
+++ admin/scripts/login.php
@@ -8,9 +8,16 @@ function login($username, $password){
     //that counts how many entries from tbl_user
     //that has user_name match $username
 
-    $check_exist_query = 'SELECT COUNT (*) FROM tbl_user WHERE user_name = '.$username;
+    $check_exist_query = 'SELECT COUNT(*) FROM tbl_user WHERE user_name = :username';
+
+    echo $check_exist_query;
 
     $user_set = $pdo->prepare($check_exist_query);
+    $user_set->execute(
+        array(
+            ':username'=>$username
+        )
+    );
 
     if($user_set->fetchColumn()>0){
         echo 'User Exists!';
