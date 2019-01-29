10d2bdb9ea83a68ba245602fd24b506e432b9608
jaclynflomen <j_floman@fanshaweonline.ca>
2019-01-29 15:26:16 -0500

    logged in

--- admin/scripts/login.php
+++ admin/scripts/login.php
@@ -10,8 +10,7 @@ function login($username, $password){
 
     $check_exist_query = 'SELECT COUNT(*) FROM tbl_user WHERE user_name = :username';
 
-    echo $check_exist_query;
-
+    // echo $check_exist_query;
     $user_set = $pdo->prepare($check_exist_query);
     $user_set->execute(
         array(
@@ -19,8 +18,25 @@ function login($username, $password){
         )
     );
 
+    //TODO: fill the following lines with the proper SQL query so that it can get all rows where 
+    //user_name = $username and user_pass = $password
     if($user_set->fetchColumn()>0){
-        echo 'User Exists!';
+        $get_user_query = 'SELECT * FROM tbl_user WHERE user_name = :username AND user_pass = :password';
+
+        $get_user_set = $pdo->prepare($get_user_query);
+
+        $get_user_set->execute(
+            array(
+                ':username'=>$username,
+                ':password'=>$password
+            )
+        );
+
+        while($found_user = $get_user_set->fetch(PDO::FETCH_ASSOC)){
+            $id = $found_user['user_id'];
+
+            echo 'User ID: '.$id. 'Logged in!';
+        }
     }
 
 }
